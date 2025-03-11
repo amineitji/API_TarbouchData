b67ca58805547675814b416a597a2c9c55fccd53
amineitji <amineitji@gmail.com>
2025-03-11 09:23:33 +0100

    MAJ - debut

--- API_TarbouchData/API_TarbouchData/settings.py
+++ API_TarbouchData/API_TarbouchData/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+# Import dj-database-url at the beginning of the file.
+import dj_database_url
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_0rmh12(&@#)7s6t$zy*94n!f@423f#f(8^k0o@nzdy&=$uphj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]  # Pas sécurisé, à limiter si besoin
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 
 # Application definition
@@ -76,8 +80,7 @@ WSGI_APPLICATION = 'API_TarbouchData.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy',
     }
 }
 
