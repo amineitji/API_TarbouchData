1595ef0b1a1b929b993a556e52e215fb5267c4ff
Amine <100621612+amineitji@users.noreply.github.com>
2025-03-13 11:37:10 +0100

    Update wsgi.py

--- API_TarbouchData/API_TarbouchData/wsgi.py
+++ API_TarbouchData/API_TarbouchData/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', ' API_TarbouchData.API_TarbouchData.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'API_TarbouchData.settings')
 
 application = get_wsgi_application()
