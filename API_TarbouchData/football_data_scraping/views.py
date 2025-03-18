b2c029e5fd439db857d81fa0d47fc5c4560c63ea
amineitji <amineitji@gmail.com>
2025-03-18 10:55:19 +0100

    MAJ - restframework

--- API_TarbouchData/football_data_scraping/views.py
+++ API_TarbouchData/football_data_scraping/views.py
@@ -9,15 +9,19 @@ import json
 @parser_classes([JSONParser])
 def extract_whoscored_data(request):
     try:
+        # Extraire les paramètres du corps de la requête
         player_name = request.data.get("player_name")
         html_path = request.data.get("html_path")
-        
+
+        # Vérifier que les paramètres sont présents
         if not player_name or not html_path:
             return Response({"error": "Missing required parameters: 'player_name' and 'html_path'."}, status=status.HTTP_400_BAD_REQUEST)
         
+        # Effectuer l'extraction des données
         extractor = WhoScoredDataExtractor(html_path)
         data = extractor.extract_player_stats_and_events(player_name)
         
+        # Retourner la réponse
         response = Response(json.loads(data), status=status.HTTP_200_OK)
         response['Content-Disposition'] = f'attachment; filename="{player_name.replace(" ", "_")}_data.json"'
         return response
